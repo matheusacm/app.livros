24e061acdb969fda9953d266b763e75967d31038
matheusacm <matheusfragoso040820@gmail.co>
2026-05-30 11:04:59 -0300

    serviços estaticos

--- src/js/componentes/paginas/servicos.js
+++ src/js/componentes/paginas/servicos.js
@@ -1,4 +1,33 @@
-const servicos = `<h1> Esta é página serviços
-<p>Nossos serviços incluem: </p>`
+const servicos = `<h1> Esta é página servicos </h1>
+     <div class="bem-grid-auto">
+     <div class="bem-card">
+      <img class="bem-card__image" src="image.jpg" alt="Image description">
+      <div class="bem-card__body">
+        <h3 class="bem-card__title">Title</h3>
+        <p>Description</p>
+      </div>
+     </div>
+      <div class="bem-card">
+      <img class="bem-card__image" src="image.jpg" alt="Image description">
+      <div class="bem-card__body">
+        <h3 class="bem-card__title">Title</h3>
+        <p>Description</p>
+      </div>
+     </div>
+     <div class="bem-card">
+      <img class="bem-card__image" src="image.jpg" alt="Image description">
+      <div class="bem-card__body">
+        <h3 class="bem-card__title">Title</h3>
+        <p>Description</p>
+      </div>
+     </div>
+     <div class="bem-card">
+      <img class="bem-card__image" src="image.jpg" alt="Image description">
+      <div class="bem-card__body">
+        <h3 class="bem-card__title">Title</h3>
+        <p>Description</p>
+      </div>
+     </div>
+    </div>`
 
 export default servicos;
